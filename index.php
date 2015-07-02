84802be29dd675a5764c9271c67b1d6cceb74258
leandross2 <ls-saraiva2@hotmail.com>
2015-07-02 00:48:10 -0300

    ESTRUTURA tela Login

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css"> 
+	<title>Login</title>
+</head>
+<body>
+	<div id="interface">
+		<div class="logo">
+			<img src="img/icone_entrada.jpg" alt="entrada" />
+		</div>
+		<div class="boxLogin">
+			<p class="tituloForm">Nome Projeto</p>
+			<form action="" method="" id="formLogin">
+				<input type="text" id="usuario" placeholder="usuário" />
+				<input type="password" id="senha" placeholder="Senha" />
+				<input type="submit" class="submit">
+			</form>
+		</div>
+	</div>
+</body>
+</html>
