903422fb7019c3f78ed07483488c86e476cebedc
leandross2 <ls-saraiva2@hotmail.com>
2015-07-01 23:53:32 -0300

    prototipo login

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<title>Login</title>
+</head>
+<body>
+	<div id="interface">
+		<div class="logo">
+			<img src="" alt="" />
+		</div>
+		<div class="boxLogin">
+			<p class="chamada">Nome projeto</p>
+			<input type="text" id="usuario"   />
+			<input type="password" id="senha" />
+		</div>
+	</div>
+</body>
+</html>
