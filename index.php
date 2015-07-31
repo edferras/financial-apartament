44b1cfa761974468c8d3fa33bfe4a996e647bf9f
rsantosrss <tenorioxp@gmail.com>
2015-07-31 20:57:03 -0300

    Criação da tela de login.

--- index.php
+++ index.php
@@ -2,21 +2,30 @@
 <html lang="pt-BR">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="style.css"> 
 	<title>Login</title>
 </head>
 <body>
 	<div id="interface">
 		<div class="logo">
-			<img src="img/icone_entrada.jpg" alt="entrada" />
+			<img src="img/money.png" alt="entrada" />
 		</div>
 		<div class="boxLogin">
 			<p class="tituloForm">Nome Projeto</p>
-			<form action="" method="" id="formLogin">
-				<input type="text" id="usuario" placeholder="usuário" />
+			<form action="" method="post" id="formLogin">
+				<input type="text" id="usuario" placeholder="Usuário" />
 				<input type="password" id="senha" placeholder="Senha" />
-				<input type="submit" class="submit">
-			</form>
+				<input type="submit" class="submit" value="Entrar">
+			</form>			
+		</div>
+		<div class="boxCadastro">
+		    <div id="esqueceuSenha">
+		      <p>Senha ?</p>
+		    </div>
+		    <div id="registraLogin">
+		      <p>Registrar-se</p>
+		    </div> 
 		</div>
 	</div>
 </body>
